3980912c8b7a4f9f992576e65f3a28a1fe32af49
berkin <berkin.cirak@comeon.com>
2016-12-23 11:05:08 +0300

    react start

---
+++ components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return <div>xxx</div>;
+    }
+}
+
+export default App;
