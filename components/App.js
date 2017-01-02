acc409272ca75928b0a4c54cdab8f6e5487f3f90
berkin <berkin.cirak@comeon.com>
2017-01-02 16:59:22 +0300

    react clean

--- components/App.js
+++ components/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import Profile from './Profile';
-import EducationList from './EducationList';
-import ExperienceList from './ExperienceList';
+import data from '../data/resume.json';
 
-class App extends React.Component {
-	render() {
-		return (
-			<div>
-				<Profile />
-				<EducationList />
-				<ExperienceList />
-			</div>);
-	}
-}
+
+const App = () => (
+	<div>
+		<h1>Profile</h1>
+		<div>Name: {data.name}</div>
+		<div>Title: {data.title}</div>
+		<div><img src={data.avatar} alt={data.name} /></div>
+		<div>email: {data.email}</div>
+		<div>phone: {data.phone}l</div>
+
+		<h1>Education</h1>
+		<ul>
+			{
+			data.education.map((item, index) =>
+				<li key={index}>{item.school} - {item.faculty}</li>
+			)
+			}
+		</ul>
+
+		<h1>Experiences</h1>
+		<ul>
+			{
+			data.experiences.map((item, index) =>
+				<li key={index}>{item.begin} - {item.end} {item.company} {item.title} {item.description}</li>
+			)
+			}
+		</ul>
+	</div>
+);
 
 export default App;
