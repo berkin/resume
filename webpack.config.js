01ea660b0988a90e79c635457c6d961aab734abb
berkin <berkin.cirak@comeon.com>
2017-02-27 10:08:26 +0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = [
 		output: {
 			path: path.join(__dirname, 'dist/assets'),
 			filename: 'bundle.js',
-			publicPath: '/assets/'
+			publicPath: 'assets/'
 		},
 		devtool: 'source-map',
 		devServer: {
